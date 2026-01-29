10dbad34b87e93d6f9930aa3363bc12fa3107ab0
Martin Algesten <martin@lookback.io>
2026-01-29 17:37:36 +0100

    zed

--- config/nvim/lua/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- define common options
-local opts = {
-    noremap = true,      -- non-recursive
-    silent = true,       -- do not show message
-}
-
-vim.keymap.set({'n', 'v', 'i'}, '<C-a>', '<Home>', opts)
-vim.keymap.set({'n', 'v', 'i'}, '<C-e>', '<End>', opts)
-
